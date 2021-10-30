db1d5b8d074f2f5fbd7afae93394cf4ae1841a76
202530 <dgladushenko@mail.ru>
2021-10-30 14:13:36 +0300

    Deleted

--- Test_first/First_programm.R
+++
@@ -1,8 +0,0 @@
-print("Hello World", quote=FALSE)
-# plot(sample(1:20))
-
-?demo()
-
-demo()
-
-demo(graphics)
--- Test_first/Graphics.R
+++
@@ -1,7 +0,0 @@
-# ?demo()
-# plot(sample(1:20))
-# demo(graphics)
-dev.new(demo(graphics))
-
-
-
