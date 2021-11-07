8beb45246e1b3f334d109c61668aa71edd803784
202530 <dgladushenko@mail.ru>
2021-11-08 02:05:50 +0300

    Init Lab_4 User_funcs № 1

--- Lab_4/test.R
+++ Lab_4/test.R
@@ -1,13 +1,16 @@
 
 pow <- function (x=10L, y=2, z=2){
-  if (is.double(x) || is.integer(x) && (is.double(y) || is.integer(y)) && (is.double(z) || is.integer(z))){
+  vec_check <- c(x, y, z)
+  # if (is.double(x) || is.integer(x) && (is.double(y) || is.integer(y)) && (is.double(z) || is.integer(z))){
+  if (is.numeric(vec_check)){
+    print("Checked, processing...", quote = FALSE)
     if (z!=0){
       x**y/z
     } else{
-      print("Делить на ноль нельзя! Dont")
+      print("You cannot divide by zero! z = 0 (Error)")
     }
   } else {
-    print("0")
+    print("Some numbers is not numeric")
   }
 }
 
--- Lab_4/userFuncs.R
+++ Lab_4/userFuncs.R
@@ -1,5 +1,11 @@
-setwd("C:\\Users\\202530\\DataspellProjects\\R_works\\R_lessons_2")
+setwd("C:\\Users\\202530\\DataspellProjects\\R_works\\R_lessons_new\\Lab_4")
 source("test.R")
-fe <- "Hi"
-pow(10L, , fe) # 15
+# № 1
+TRUe <- "Hi"
+pow(10L, FALSE, TRUe) # "Some numbers is not numeric"
+pow(10L, 5, FALSE) # "You cannot divide by zero! z = 0 (Error)"
+pow(10L, 2, 10) # 10
+
+# № 2-5
+N <- c(1, 2, 3)
 
