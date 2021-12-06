44cafed3cc67e1891a327418d59e398ccf5f8f1e
202530 <dgladushenko@mail.ru>
2021-12-07 00:10:06 +0300

    several goods!

--- KR_1/test.R
+++ KR_1/test.R
@@ -4,64 +4,108 @@ cat("---------------------------------------------------")
 setwd("C:/Users/202530/DataspellProjects/R_lessons/KR_1")
 print(getwd(), quote = FALSE)
 print("Заполнение и копирование исходных данных...", quote = FALSE)
-generate.data <- function(n=10, mi=20, ma=100, filename = "Глад", loc_path = 'Гладушенко_Даниил_ПИ20-1/Магазин'){
-  for (k in 1:10){
+
+goods <- list(c(n=10, mi=20, ma=100, name="Сыр, кг"), c(n=10, mi=40, ma=80, name="Молоко,л"))
+
+write_sup <- function (days, goods=list(c(n=10, mi=20, ma=100, name="Сыр,кг")), loc_path, sep=.Platform$file.sep, k, filename, value_sup, EXT){
+  n <-  as.integer(goods[[1]][[1]][1])
+  days <- c(1:n)
+  Data <- data.frame('День недели' = days)
+  for (vec in goods){
+    n <-  as.integer(vec[1])
+    mi <-  as.integer(vec[2])
+    ma <-  as.integer(vec[3])
+    name <- as.character(vec[length(vec)])
+
+    # Создание двух векторов - доставки и продажи товара соотв. за n дней
     value_sup <- (sample(x = mi:ma, size = n, replace=TRUE))
 
     value_sal <- c()
     for (elem in value_sup){
       value_sal <- c(value_sal, sample(0:elem, size=1))
     }
-    days <- c(1:10)
+    Data[name] <- value_sup
+  }
+  # Creating sources files
+  write.table(Data,
+              paste0(loc_path, as.character(k), sep, filename, EXT, '.csv'),
+              row.names = FALSE,
+              sep = ';',
+              dec=',')
 
-    Data <- data.frame('Дни' = days, 'Поставки_сыра' = value_sup)
-      write.table(Data,
-                paste0(loc_path, as.character(k), '/', filename, '.in.csv'),
+  # Copy to Analysis
+  cp_path_sup <- paste0(loc_path, as.character(k), sep, 'Магазин', as.character(k), '_', filename, EXT, '.csv')
+  write.table(Data,
+              cp_path_sup,
+              row.names = FALSE,
+              sep = ';',
+              dec=',')
+  # Check
+  if (file.exists(paste0('Analysis', sep, 'Магазин', as.character(k), '_', filename, EXT, '.csv'))){
+    file.remove(paste0('Analysis', sep, 'Магазин', as.character(k), '_', filename, EXT, '.csv'))
+  }
+  # Delete unused files
+  file.copy(cp_path_sup, 'Analysis')
+  file.remove(cp_path_sup)
+}
+
+write_sal <- function (days, goods=list(c(n=10, mi=20, ma=100, name="Сыр, кг")), loc_path, sep=.Platform$file.sep, k, filename, value_sal, EXT){
+  n <-  as.integer(goods[[1]][[1]][1])
+  days <- c(1:n)
+  Data <- data.frame('День недели' = days)
+  for (vec in goods){
+    n <-  as.integer(vec[1])
+    mi <-  as.integer(vec[2])
+    ma <-  as.integer(vec[3])
+    name <- as.character(vec[length(vec)])
+
+    # Создание двух векторов - доставки и продажи товара соотв. за n дней
+    value_sup <- (sample(x = mi:ma, size = n, replace=TRUE))
+
+    value_sal <- c()
+    for (elem in value_sup){
+      value_sal <- c(value_sal, sample(0:elem, size=1))
+    }
+    Data[name] <- value_sal
+  }
+  # Creating sources files
+  write.table(Data,
+                paste0(loc_path, as.character(k), sep, filename, EXT, '.csv'),
                 row.names = FALSE,
                 sep = ';',
                 dec=',')
 
-
-    task <- 'Скопировать файлы с данными из папок магазинов в папку Анализ.
-При копировании файлов в папку Анализ имена файлов Микси.in, Микси.out
-из папок Магазин 1, Магазин 2 и т.д. заменять на имена Магазин1_Микси.in,
-Магазин1_Микси.out и т.д.'
-    cp_path_sup <- paste0(loc_path, as.character(k), '/', 'Магазин', as.character(k), '_', filename, '.in.csv')
-    write.table(Data,
-                cp_path_sup,
-                row.names = FALSE,
-                sep = ';',
-                dec=',')
-      if (file.exists(paste0('Analysis', '/', 'Магазин', as.character(k), '_', filename, '.in.csv'))){
-        file.remove(paste0('Analysis', '/', 'Магазин', as.character(k), '_', filename, '.in.csv'))
-      }
-      file.copy(cp_path_sup, 'Analysis')
-      file.remove(cp_path_sup)
-
-    Data <- data.frame('Дни' = days, 'Продажи_сыра' = value_sal)
-    write.table(Data,
-                paste0(loc_path, as.character(k), '/', filename, '.out.csv'),
-                row.names = FALSE,
-                sep = ';',
-                dec=',')
-    cp_path_sal <- paste0(loc_path, as.character(k), '/', 'Магазин', as.character(k), '_', filename, '.out.csv', sep = '')
-    write.table(Data,
+  # Copy to Analysis
+  cp_path_sal <- paste0(loc_path, as.character(k), sep, 'Магазин', as.character(k), '_', filename, EXT, '.csv')
+  write.table(Data,
                 cp_path_sal,
                 row.names = FALSE,
                 sep = ';',
                 dec=',')
-    if (file.exists(paste0('Analysis', '/', 'Магазин', as.character(k), '_', filename, '.out.csv'))){
-      file.remove(paste0('Analysis', '/', 'Магазин', as.character(k), '_', filename, '.out.csv'))
+  # Check
+  if (file.exists(paste0('Analysis', sep, 'Магазин', as.character(k), '_', filename, EXT, '.csv'))){
+    file.remove(paste0('Analysis', sep, 'Магазин', as.character(k), '_', filename, EXT, '.csv'))
     }
+  # Delete unused files
+  file.copy(cp_path_sal, 'Analysis')
+  file.remove(cp_path_sal)
 
-    file.copy(cp_path_sal, 'Analysis')
-    file.remove(cp_path_sal)
+}
 
+generate.data <- function(goods, filename = "Глад", loc_path = 'Гладушенко_Даниил_ПИ20-1/Магазин'){
+  EXT_SUPPLY <- '.in'
+  EXT_SALE <- '.out'
+  for (k in 1:10){
+    # print(goods, quote=FALSE)
+    # Создание таблицы
+
+    write_sup(days, goods, loc_path, sep=.Platform$file.sep, k, filename, value_sup, EXT_SUPPLY)
+    write_sal(days, goods, loc_path, sep=.Platform$file.sep, k, filename, value_sal, EXT_SALE)
     }
 }
 
 # Заполнение пустых csv файлов данными о поставках и продажах
-generate.data()
+generate.data(goods)
 
 print("Переход в папку Analysis", quote = FALSE)
 setwd("C:/Users/202530/DataspellProjects/R_lessons/KR_1/Analysis")
@@ -95,27 +139,27 @@ in5
 out5
 
 print("All is OK")
-colnames(in1)[colnames(in1) == "Дни"] <- "День недели"
-colnames(in2)[colnames(in2) == "Дни"] <- "День недели"
-colnames(in3)[colnames(in3) == "Дни"] <- "День недели"
-colnames(in4)[colnames(in4) == "Дни"] <- "День недели"
-colnames(in5)[colnames(in5) == "Дни"] <- "День недели"
-colnames(in6)[colnames(in6) == "Дни"] <- "День недели"
-colnames(in7)[colnames(in7) == "Дни"] <- "День недели"
-colnames(in8)[colnames(in8) == "Дни"] <- "День недели"
-colnames(in9)[colnames(in9) == "Дни"] <- "День недели"
-colnames(in10)[colnames(in10) == "Дни"] <- "День недели"
-
-colnames(out1)[colnames(out1) == "Дни"] <- "День недели"
-colnames(out2)[colnames(out2) == "Дни"] <- "День недели"
-colnames(out3)[colnames(out3) == "Дни"] <- "День недели"
-colnames(out4)[colnames(out4) == "Дни"] <- "День недели"
-colnames(out5)[colnames(out5) == "Дни"] <- "День недели"
-colnames(out6)[colnames(out6) == "Дни"] <- "День недели"
-colnames(out7)[colnames(out7) == "Дни"] <- "День недели"
-colnames(out8)[colnames(out8) == "Дни"] <- "День недели"
-colnames(out9)[colnames(out9) == "Дни"] <- "День недели"
-colnames(out10)[colnames(out10) == "Дни"] <- "День недели"
+# colnames(in1)[colnames(in1) == "Дни"] <- "День недели"
+# colnames(in2)[colnames(in2) == "Дни"] <- "День недели"
+# colnames(in3)[colnames(in3) == "Дни"] <- "День недели"
+# colnames(in4)[colnames(in4) == "Дни"] <- "День недели"
+# colnames(in5)[colnames(in5) == "Дни"] <- "День недели"
+# colnames(in6)[colnames(in6) == "Дни"] <- "День недели"
+# colnames(in7)[colnames(in7) == "Дни"] <- "День недели"
+# colnames(in8)[colnames(in8) == "Дни"] <- "День недели"
+# colnames(in9)[colnames(in9) == "Дни"] <- "День недели"
+# colnames(in10)[colnames(in10) == "Дни"] <- "День недели"
+#
+# colnames(out1)[colnames(out1) == "Дни"] <- "День недели"
+# colnames(out2)[colnames(out2) == "Дни"] <- "День недели"
+# colnames(out3)[colnames(out3) == "Дни"] <- "День недели"
+# colnames(out4)[colnames(out4) == "Дни"] <- "День недели"
+# colnames(out5)[colnames(out5) == "Дни"] <- "День недели"
+# colnames(out6)[colnames(out6) == "Дни"] <- "День недели"
+# colnames(out7)[colnames(out7) == "Дни"] <- "День недели"
+# colnames(out8)[colnames(out8) == "Дни"] <- "День недели"
+# colnames(out9)[colnames(out9) == "Дни"] <- "День недели"
+# colnames(out10)[colnames(out10) == "Дни"] <- "День недели"
 
 
 in5[1]
@@ -235,4 +279,3 @@ write.table(res.tab, 'Результаты.csv', row.names = FALSE, sep = ';')
 if (file.exists("Результаты.csv")){
   print("Таблица создана!", quote = FALSE)
 }
-werter
