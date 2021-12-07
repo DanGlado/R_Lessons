cd17e5d9908d8338d847f80f3b2418df39ab3d77
202530 <dgladushenko@mail.ru>
2021-12-07 21:48:34 +0300

    Good Job, Dan! Done Table

--- KR_1/test.R
+++ KR_1/test.R
@@ -5,9 +5,9 @@ setwd("C:/Users/202530/DataspellProjects/R_lessons/KR_1")
 print(getwd(), quote = FALSE)
 print("Заполнение и копирование исходных данных...", quote = FALSE)
 
-goods <- list(c(n=10, mi=20, ma=100, name="Сыр, кг"), c(n=10, mi=40, ma=80, name="Молоко,л"))
+goods <- list(c(n=10, mi=20, ma=100, name="Сыр"), c(n=10, mi=20, ma=100, name="Молоко"))
 
-write_data <- function (days, goods=list(c(n=10, mi=20, ma=100, name="Сыр,кг")), loc_path, sep=.Platform$file.sep, k, filename){
+write_data <- function (days, goods=list(c(n=10, mi=20, ma=100, name="Сыр")), loc_path, sep=.Platform$file.sep, k, filename){
   EXT_SUPPLY <- '.in'
   EXT_SALE <- '.out'
   n <-  as.integer(goods[[1]][[1]][1])
@@ -176,34 +176,65 @@ realiz <- c()
 util <- c()
 srd <- c()
 
-VARS_in <- list(in1[, 2], in2[, 2], in3[, 2], in4[, 2], in5[, 2], in6[, 2], in7[, 2], in8[, 2], in9[, 2], in10[, 2])
-VARS_out <- list(out1[, 2], out2[, 2], out3[, 2], out4[, 2], out5[, 2], out6[, 2], out7[, 2], out8[, 2], out9[, 2], out10[, 2])
+VARS_in <- list(in1, in2, in3, in4, in5, in6, in7, in8, in9, in10)
+VARS_out <- list(out1, out2, out3, out4, out5, out6, out7, out8, out9, out10)
 
 P_supply <- 5500
 P_sale <- 8000
 P_util <- 400
 
 
-VARS_in[[1]]
+# VARS_in[[1]]
 # test <- VARS_in[1] - VARS_out[1]
 # test
 # dev.new(); par(mfrow = c(strings, columns)); f <- factor(x = flex, levels = day.week);
-for (n in 1:10){
-  Q_util <- sum(VARS_in[[n]]) - sum(VARS_out[[n]])
-  gain <- c(gain, sum(VARS_out[[n]]) * P_sale)
-  profit <- c(profit, gain[n] - (sum(VARS_in[[n]]) + P_util * Q_util))
-  realiz <- c(realiz, sum(VARS_out[[n]]))
-  util <- c(util, Q_util)
-  srd <- c(srd, sd(VARS_out[[n]]))
+sum(VARS_in[[1]][, 2])
+# print(VARS_in)
+Q_util <- c()
+exes <- c()
 
+
+for (n in 1:10){
+  for (col in 2:(length(goods)+1)){
+    v.prod.in <- VARS_in[[n]][, col]
+    v.prod.out <- VARS_out[[n]][, col]
+
+    gain <- c(gain, sum(v.prod.out) * P_sale)
+    realiz <- c(realiz, sum(v.prod.out))
+    srd <- c(srd, sd(v.prod.out))
+
+    Q_util <- sum(v.prod.in) - sum(v.prod.out)
+    util <- c(util, Q_util)
+    exes <- c(exes, (P_util * Q_util + P_supply * sum(v.prod.in)))
+  }
+}
+gain_test <- gain
+exes_test <- exes
+util_test <- util
+profit <- c(profit, (gain - exes))
+steps <- seq(from = 1, to = length(goods)*n, by = length(goods))
+print(steps)
+result.week <- function(vector){
+  final.vec <- c()
+  for (st in steps){
+    final.vec <- c(final.vec, sum(c(vector[st], vector[st+1])))
+  }
+  return (final.vec)
 }
 
-print(c("gain ", gain), quote = FALSE)
+if (length(goods) > 1){
+  gain <- result.week(gain)
+  profit <- result.week(profit)
+  realiz <- result.week(realiz)
+  util <- result.week(util)
+  srd <- result.week(srd)
+}
+
+print(c("profit ", gain), quote = FALSE)
 print(c("profit ", profit), quote = FALSE)
 print(c("realiz ", realiz), quote = FALSE)
 print(c("util ", util), quote = FALSE)
 print(c("sd ", srd), quote = FALSE)
-
 
 sale_max <- c()
 sale_min <- c()
@@ -212,26 +243,50 @@ days1 <- c()
 days2 <- c()
 days3 <- c()
 
-for (n in 1:10){
-  s_ma <- max(VARS_out[[n]])
-  day1 <- which.max(VARS_out[[n]])
-  s_mi <- min(VARS_out[[n]])
-  day2 <- which.min(VARS_out[[n]])
-  u_ma <- max(VARS_in[[n]]-VARS_out[[n]])
-  day3 <- which.max(VARS_in[[n]]-VARS_out[[n]])
+s_ma <- c()
+s_mi <- c()
+u_ma <- c()
 
-  sale_max <- c(sale_max, s_ma)
-  sale_min <- c(sale_min, s_mi)
-  util_max <- c(util_max, u_ma)
+selected.prod <- readline(prompt="Enter product: ")
+
+name.goods <- c()
+for (good in goods){
+  name.goods <- c(name.goods, good[length(good)])
+}
+
+
+while (!(selected.prod %in% name.goods)){
+  print("Товар не найден! Попробуйте снова")
+  selected.prod <- readline(prompt="Enter product: ")
+}
+
+print("Товар найден!")
+ind_col <- as.integer(which(name.goods == selected.prod)+1)
+for (n in 1:10){
+  v.prod.in <- VARS_in[[n]][, ind_col]
+  v.prod.out <- VARS_out[[n]][, ind_col]
+
+  s_ma <- c(s_ma, max(v.prod.out))
+  day1 <- which.max(v.prod.out)
+  s_mi <- c(s_mi, min(v.prod.out))
+  day2 <- which.min(v.prod.out)
+  u_ma <- c(u_ma, max(v.prod.in-v.prod.out))
+  day3 <- which.max(v.prod.in-v.prod.out)
 
   days1 <- c(days1, day1)
   days2 <- c(days2, day2)
   days3 <- c(days3, day3)
+
 }
-print(VARS_out)
-print(c(sale_max, days1))
-print(c(sale_min, days2))
-print(c(util_max, days3))
+  print(s_ma, day1)
+  # sale_max <- c(sale_max, s_ma)
+  # sale_min <- c(sale_min, s_mi)
+  # util_max <- c(util_max, u_ma)
+
+# print(VARS_out)
+# print(c(sale_max, days1))
+# print(c(sale_min, days2))
+# print(c(util_max, days3))
 
 
 res.tab[2] <- c('За нед', gain, sum(gain), mean(gain))
@@ -240,11 +295,11 @@ res.tab[4] <- c('За нед', realiz, sum(realiz),  mean(realiz))
 res.tab[5] <- c('За нед', util, sum(util), mean(util))
 res.tab[6] <- c('За нед', srd, sum(srd), mean(srd))
 
-res.tab[7] <- c('За день', sale_max, NA, NA)
+res.tab[7] <- c('За день', s_ma, rep(NA, 2))
 res.tab[8] <- c('№ Дня', days1, NA, NA)
-res.tab[9] <- c('За день', sale_min, NA, NA)
+res.tab[9] <- c('За день', s_mi, NA, NA)
 res.tab[10] <- c('№ Дня', days2, NA, NA)
-res.tab[11] <- c('За день', util_max, NA, NA)
+res.tab[11] <- c('За день', u_ma, NA, NA)
 res.tab[12] <- c('№ Дня', days3, NA, NA)
 
 print(res.tab)
